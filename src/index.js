ad23401686c56796e79bdad6fd3b6ca204e9b722
Izuku0 <74543643+Izuku0@users.noreply.github.com>
2024-10-08 21:27:08 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -9,7 +9,16 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Mongo Db connection Failed!!!",err);
+    
+})
 
 
 
