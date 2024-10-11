a6a69e2afeb7fcf41104971dc701ada948a6cd34
Izuku0 <74543643+Izuku0@users.noreply.github.com>
2024-10-11 13:33:27 +0530

    setup config

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
-class ApiRespose{
-    constructor(statusCode,data,message ="Success"){
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
         this.statusCode = statusCode
         this.data = data
         this.message = message
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,12 @@
-const asyncHandler =(requestHandler) =>{
-    return async (req,res,next) =>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err) =>next(err))
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
 
 
-export {asyncHandler}
+
 
 
 // const asyncHandler =(fn) => async (req,res,next) =>{
@@ -20,3 +20,4 @@ export {asyncHandler}
 //     }
 // }
 
+export {asyncHandler}
