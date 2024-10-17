ea7b7925470f523c9a4f95275b6c3962af8a1a4d
Izuku0 <74543643+Izuku0@users.noreply.github.com>
2024-10-17 15:21:35 +0530

    added routes and refactored

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from "./app.js";
-
-
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
-
-
+  path: "./env",
+});
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running at port: ${process.env.PORT || 8000}`);
-        
-    })
-})
-.catch((err)=>{
-    console.log("Mongo Db connection Failed!!!",err);
-    
-})
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo Db connection Failed!!!", err);
+  });
 
 /*
 ;( async ()=>{
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,28 +1,51 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken} from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
-const router = Router()
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentUserPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getWatchHistory,
+  getUserChannelProfile,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+const router = Router();
 
 router.route("/register").post(
-   upload.fields([
+  upload.fields([
     {
-        name:"avatar",
-        maxCount:1
+      name: "avatar",
+      maxCount: 1,
     },
     {
-        name:"coverImage",
-        maxCount:1
-    }
-   ]),
-    registerUser
-)
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
 
-export default router
+router.route("/change-password").post(verifyJWT, changeCurrentUserPassword);
+router.route("/profile").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/coverImage")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+export default router;
